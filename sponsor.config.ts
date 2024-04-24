f96326dd81c4c1eec0ed57e3cef3a7989d1a6d38
Coywolfboy <99562268+Coywolfboy@users.noreply.github.com>
2024-04-24 15:11:22 +0200

    Add files via upload

---
+++ sponsor.config.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { defineConfig, presets } from 'sponsorkit'
+
+export default defineConfig({
+  github: {
+    login: 'Coywolfboy',
+    type: 'user',
+  },
+  formats: ['json', 'svg'],
+  tiers: [
+    {
+      title: 'Sponsors ❤️',
+      preset: presets.base,
+    },
+    {
+      title: 'Silver Sponsors 💖',
+      monthlyDollars: 5,
+      preset: presets.medium,
+      // to insert custom elements after the tier block
+      composeAfter: (composer, tierSponsors, config) => {
+        composer.addSpan(10)
+      },
+    },
+    {
+      title: 'Gold Sponsors 🧡',
+      monthlyDollars: 10,
+      preset: presets.large,
+    },
+    {
+      title: 'Platinum Sponsors 🤍',
+      monthlyDollars: 20,
+      preset: presets.xl,
+    },
+  ],
+})
